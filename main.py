718989f4a33ab967c94da73204a86703ac72e956
MikeySz <60848730+MikeySz@users.noreply.github.com>
2022-03-10 10:47:45 -0500

    Basic Architecure
    This is most likely the "skeleton" of our system. Most of it should be similarly  structured.

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 #Main Python File used to run the app and call in other classes/method from the Classes folder
 #Team AMCJ
 #Kean Senior Captstone
+#All Logic should be here
+#.KV files should hold the interface/gui
 #===========================================
 #Imports-------------------------
-import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+
 
 #--------------------------------
+class HomeScreen(Screen):
+	pass
+
+class SettingsScreen(Screen):
+	pass
+
+
+
 GUI = Builder.load_file("main.kv")
-
-
 class MyApp(App):
 	def build(self):
 		return GUI
+
+	def change_screen(self,screen_name,transition,direction):
+		# Screen manager from main.kv
+		#print(self.root.ids) #Code used to test the screen_name id
+		
+		#when change_screen is called, it grabs the screen_id
+		#from the page(screen) calling the change_screen method
+		#sets the id to match the new id
+		screen_manager = self.root.ids['screen_manager']
+		
+		screen_manager.current = screen_name
+		#
+
 
 #Runs the application
 if __name__ == '__main__':
